68888f158cf98d6bb8fed6fadc83403829b75918
Tsun Ki Lam <lamt@uni.coventry.ac.uk>
2017-03-21 18:24:04 +0000

    template

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template
+import os
 
 app = Flask(__name__);
 
-
 @app.route('/')
 def send_static():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
-@app.route('/<path:path>')
-def send_static2(path):
-    return send_from_directory('static', path)
+@app.route('/photoshop')
+def photoshop():
+    return render_template('Photoshop.html')
 
 if __name__ == '__main__':
-   app.run(port=5000, host='127.0.0.1', debug=True)
-    # app.run()
+    app.debug = True
+    port = int(os.getenv('PORT',8080))
+    host = os.getenv('IP', '0.0.0.0')
+    app.run()
