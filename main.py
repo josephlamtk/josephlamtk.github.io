f6c936033dd26db72bbf19721fec562e8e0c7bbc
Tsun Ki Lam <lamt@uni.coventry.ac.uk>
2017-04-06 00:35:09 +0100

    idk

--- main.py
+++ main.py
@@ -1,11 +1,51 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory, render_template, redirect, url_for
+from flask_wtf import Form
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length
+from flask_bootstrap import Bootstrap
 import os
+import sqlite3
 
 app = Flask(__name__);
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'comments.db'),
+    SECRET_KEY='development key'
+))
+
+Bootstrap(app)
+
+class CommentForm(Form):
+    name = StringField('Name:', validators=[DataRequired()])
+    comments = TextAreaField('Comments', validators=[DataRequired(), Length(min=3, max=10)])
+    submit = SubmitField('Submit')
 
 @app.route('/')
 def send_static():
     return render_template('index.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = CommentForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        comments = form.comments.data
+        with sqlite3.connect(app.config['DATABASE']) as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO comments_table (name, comments) VALUES (?,?)", (name, comments))
+            con.commit()
+
+        return redirect(url_for('list_results'))
+    return render_template('form_wtf.html', form=form)
+
+@app.route('/display')
+def list_results():
+    with sqlite3.connect(app.config['DATABASE']) as con:
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute("SELECT * FROM comments_table")
+        entries = cur.fetchall()
+        return render_template('flask_sqlite.html', entries=entries)
+
 
 @app.route('/photoshop')
 def photoshop():
@@ -13,6 +53,6 @@ def photoshop():
 
 if __name__ == '__main__':
     app.debug = True
-    port = int(os.getenv('PORT',8080))
-    host = os.getenv('IP', '0.0.0.0')
+    # port = int(os.getenv('PORT',8080))
+    # host = os.getenv('IP', '0.0.0.0')
     app.run()
